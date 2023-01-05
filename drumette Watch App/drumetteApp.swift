ef817de92add8070848791a30faf6b3d0b5e3884
supersweet-dev <jim@supersweet.dev>
2023-01-05 12:00:34 -0600

    Initial Commit

---
+++ drumette Watch App/drumetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  drumetteApp.swift
+//  drumette Watch App
+//
+//  Created by Jim Alvarez on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct drumette_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
